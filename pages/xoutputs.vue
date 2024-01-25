e70d9b7c8a1fbdb767fc48f7015419132930073c
Linos Nchena <linosnchena@gmail.om>
2024-01-25 02:37:12 +0100

    front-page dedited

---
+++ pages/xoutputs.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>3 - OUTPUT DATA</h1>
+        <p>This is the about page content.</p>
+        <MyComponent />
+    </div>
+</template>
+
+
+<script setup>
+import MyComponent from '@/components/TableOutputs.vue';
+</script>
